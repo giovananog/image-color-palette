b5f7748aed9b7f46f8bd7eb1e586181d31dd4714
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-23 22:45:41 -0300

    feat: upload and show image on screen done

--- server.py
+++ server.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello world'
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def index_page():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+                return 'Nenhum arquivo enviado!'
+            
+        file = request.files['file']
+        
+        if file.filename == '':
+                return 'Nenhum arquivo selecionado!'
+        
+        if file:
+            # Salva o arquivo no diretório de uploads
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            # return 'Upload realizado com sucesso! Caminho: ' + file_path
+            return render_template('index.html', filename=file_path)
+    
+    else:
+        return render_template('index.html')
+
+@app.route('/image')
+def image_show_page():
+    return render_template('show-img.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
